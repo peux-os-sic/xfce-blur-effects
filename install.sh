3b78e05093a81b56f2ba7f3ccd3d3cb85d9472c0
DN-debug <75267055+DN-debug@users.noreply.github.com>
2021-04-29 17:26:50 +0530

    commit

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-PASSWD="$(zenity --password --title=Authentication)\n"
+###########################################################
+# Author - DN-debug
+# Description - XFWM-BLUR installation script
+###########################################################
 
-echo "changing directory"
-cd ../
-echo -e $PASSWD | sudo -S bash xfce-blur-effects/xfwm-effects-install.sh
-rm -rf xfce-blur-effects
+
+if zenity --question --text="Want to proceed?"
+then 
+    PASSWD="$(zenity --password --title=Authentication)\n"
+
+    echo "changing directory"
+    cd ../
+    echo -e $PASSWD | sudo -S bash xfce-blur-effects/xfwm-effects-install.sh
+
+else
+    notify-send "Operation skipped!"
+fi
+
+rm -rf /tmp/fetcher/xfce-blur-effects
+notify-send "Done!"
